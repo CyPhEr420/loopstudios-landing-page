94a9a1212950c23ed36a45362a3e82c873a8148d
CyPhEr420 <vimalvk9900@gmail.com>
2022-06-18 20:51:42 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const hamburger = document.querySelector('#open-btn');
+const closeBtn = document.querySelector('#close-btn');
+
+const navBar = document.querySelector('.navbar-nav');
+
+const navbarToggle = () => navBar.classList.toggle('active');
+
+hamburger.addEventListener('click', navbarToggle);
+
+closeBtn.addEventListener('click', navbarToggle);
